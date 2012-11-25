57a53146c44b8cae4c36db3cd5c19b08f36d4658
Daniel Romero <infoslack@gmail.com>
2012-11-24 23:47:50 -0300

    adicionado modificadores de frequência do CPU

---
+++ scripts/cpu_conservative.sh
@@ -0,0 +1,4 @@
+cpufreq-set -c 0 -d 799MHz -u 1.70GHz -g conservative
+cpufreq-set -c 1 -d 799MHz -u 1.70GHz -g conservative
+cpufreq-set -c 2 -d 799MHz -u 1.70GHz -g conservative
+cpufreq-set -c 3 -d 799MHz -u 1.70GHz -g conservative
---
+++ scripts/cpu_custom_ondemand.sh
@@ -0,0 +1,4 @@
+cpufreq-set -c 0 -d 799MHz -u 1.30GHz -g ondemand
+cpufreq-set -c 1 -d 799MHz -u 1.30GHz -g ondemand
+cpufreq-set -c 2 -d 799MHz -u 1.30GHz -g ondemand
+cpufreq-set -c 3 -d 799MHz -u 1.30GHz -g ondemand
