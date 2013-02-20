c11a980cf2076935f7a7a7be90791f0c4d018cfe
Daniel Romero <infoslack@gmail.com>
2013-02-20 17:06:16 -0300

    added aditional scripts in /etc/profile.d

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,7 +1,9 @@
+# System PATH
 export PATH="/opt/local/bin:/opt/local/sbin:/opt/local/ruby/gems/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 export GEM_HOME="/opt/local/ruby/gems"
 export GEM_PATH="/opt/local/ruby/gems"
 
+# Colors
 export BLUE="\[\033[0;34m\]"
 export NO_COLOR="\[\e[0m\]"
 export GRAY="\[\033[6;32;1m\]"
@@ -12,3 +14,11 @@ export LIGHT_RED="\[\033[1;31m\]"
 export RED="\[\033[0;31m\]"
 export WHITE="\[\033[1;37m\]"
 export YELLOW="\[\033[0;34;1m\]"
+
+# Append any additional sh scripts found in /etc/profile.d/ [Slackware Linux]:
+for profile_script in /etc/profile.d/*.sh ; do
+  if [ -x $profile_script ]; then
+    . $profile_script
+  fi
+done
+unset profile_script
