aa8ab54b46ce07be44e035a29e02f0dddf79e3a5
Daniel Romero <infoslack@gmail.com>
2014-04-28 21:53:13 -0300

    added  for vim

--- scripts/export.sh
+++ scripts/export.sh
@@ -10,6 +10,7 @@ export MANPATH=/usr/local/man:/usr/man
 export HOSTNAME="`cat /etc/HOSTNAME`"
 export LESSOPEN="|lesspipe.sh %s"
 export LESS="-M"
+export EDITOR="vim"
 
 # System PATH
 export PATH="/opt/local/bin:/opt/local/sbin:/opt/local/ruby/gems/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
