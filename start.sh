c71da10d6639a18b298aed0c3f72571aae4a3218
Daniel Romero <infoslack@gmail.com>
2015-01-02 21:14:55 -0300

    add bash completion for docker commands

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 source ~/.my_bash/scripts/export.sh
 source ~/.my_bash/scripts/alias.sh
 source ~/.my_bash/scripts/git-completion.sh
+source ~/.my_bash/scripts/docker.sh
 source ~/.my_bash/scripts/prompt.sh
