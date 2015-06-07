fb29d2283e249e009a9b90a265f5feca9c013772
Daniel Romero <infoslack@gmail.com>
2015-06-07 15:40:35 -0300

    add chrome container alias

--- bash/dockerfunc.sh
+++ bash/dockerfunc.sh
@@ -25,20 +25,38 @@ del_stopped(){
 # Container Aliases
 #
 http(){
-docker run --rm -t \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  --log-driver none \
-  infoslack/httpie "$@"
+  docker run --rm -t \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    --log-driver none \
+    infoslack/httpie "$@"
 }
 
 tor-browser(){
-del_stopped tor-browser
+  del_stopped tor-browser
 
-docker run -d \
-  -v /etc/localtime:/etc/localtime:ro \
-  -v /tmp/.X11-unix:/tmp/.X11-unix    \
-  -e DISPLAY=unix$DISPLAY \
-  --device /dev/snd \
-  --name tor-browser \
-  jess/tor-browser
+  docker run -d \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /tmp/.X11-unix:/tmp/.X11-unix    \
+    -e DISPLAY=unix$DISPLAY \
+    --device /dev/snd \
+    --name tor-browser \
+    jess/tor-browser
+}
+
+chrome(){
+  del_stopped chrome
+
+  docker run -d \
+    --cpuset-cpus 0 \
+    --memory 1gb \
+    --net host \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /dev/snd:/dev/snd --privileged \
+    -v /dev/dri:/dev/dri \
+    -v /tmp/.X11-unix:/tmp/ro.X11-unix \
+    -e DISPLAY=unix$DISPLAY \
+    -v $HOME/Downloads:/root/Downloads \
+    -v $HOME/.chrome:/data \
+    --name chrome \
+    jess/chrome --user-data-dir=/data "$@"
 }
